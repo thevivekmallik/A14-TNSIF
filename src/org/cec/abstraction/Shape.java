38587be10cd45ac11c4c98754c7f3e8f3b24c21c
“[thevivekmallik]” <[vivekgautam.ac@gmail.com]>
2023-09-29 16:15:08 +0530

    Fourth Commit

---
+++ src/org/cec/abstraction/Shape.java
@@ -0,0 +1,19 @@
+package org.cec.abstraction;
+
+public abstract class Shape {
+	
+	protected float area;
+	
+	 public Shape() {
+		super();
+	}
+
+	//abstract method 
+	abstract void calArea();
+	
+	 //concrete method
+	 void show()
+	 {
+		 System.out.println("Area of shape is "+area);
+	 }
+}
--- src/org/cec/usingfinal/FinalClass.java
+++ src/org/cec/usingfinal/FinalClass.java
@@ -10,10 +10,8 @@ public class FinalClass {
 class FinalChildClass extends FinalClass {
 }
 
-public class FinalClassDemo {
-	public static void main(String[] args) {
-		// Create the object of final class
-		FinalClass f1 = new FinalClass(); // Call show() method using object reference variable ab.
-		f1.show();
-	}
-}
+/*
+ * public class FinalClassDemo { public static void main(String[] args) { //
+ * Create the object of final class FinalClass f1 = new FinalClass(); // Call
+ * show() method using object reference variable ab. f1.show(); } }
+ */
