6cab1bcbcdab5535198501bec66530de90b7281f
“[thevivekmallik]” <[vivekgautam.ac@gmail.com]>
2023-09-29 15:24:47 +0530

    Second Commit

---
+++ src/org/cec/inheritance/singlelevel/SingleLevelInheritanceDemo.java
@@ -0,0 +1,11 @@
+package org.cec.inheritance.singlelevel;
+
+public class SingleLevelInheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1=new Student(101,"CEC","Vivek",1234567890,9897654334L,"Bangalore");
+	     System.out.println(s1);
+	}
+
+}
