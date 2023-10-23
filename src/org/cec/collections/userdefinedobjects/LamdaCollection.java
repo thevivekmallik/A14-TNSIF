fee230353b1d1cfd3d6b035e35b2a540021c0452
“[thevivekmallik]” <[vivekgautam.ac@gmail.com]>
2023-10-23 17:41:03 +0530

    seventh commit

---
+++ src/org/cec/collections/userdefinedobjects/LamdaCollection.java
@@ -0,0 +1,30 @@
+package org.cec.collections.userdefinedobjects;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+
+public class LamdaCollection {
+
+	public static void main(String[] args) {
+
+		ArrayList<Student> studentList = new ArrayList<Student>();
+		Student s1 = new Student(11, "Pooja", 78);
+		studentList.add(s1);
+		s1 = new Student(21, "Nikita", 85);
+		studentList.add(s1);
+		s1 = new Student(13, "Deepa", 68);
+		studentList.add(s1);
+		s1 = new Student(41, "Neha", 72);
+		studentList.add(s1);
+		System.out.println(studentList);
+		
+		//Collections.sort(studentList); //CTE
+		System.out.println("------------------------------------------");
+		Comparator<Student> comp1 = (st1, st2) -> (int)(st1.getPer() - st2.getPer());
+		Collections.sort(studentList,comp1); 
+		System.out.println("Student details in Ascending order of Per : "+studentList);
+	}
+
+}
--- src/org/cec/multithreading/synchronization/interthreadcommunication/Consumer.java
+++ src/org/cec/multithreading/synchronization/interthreadcommunication/Consumer.java
@@ -1,21 +1,23 @@
 package org.cec.multithreading.synchronization.interthreadcommunication;
 
 public class Consumer extends Thread {
-	
+
 	Q obj;
+
 	public Consumer(Q obj) {
 		this.obj = obj;
 		start();
 	}
+
 	public void run() {
 		while (true) {
-		try {
-			Thread.sleep(3000);
-		} catch (Exception e) {
-			System.out.println(e);
+			try {
+				Thread.sleep(3000);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			obj.get();
 		}
-		obj.get();
 	}
-}
-	
+
 }
